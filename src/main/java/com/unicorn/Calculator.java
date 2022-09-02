5fafa5486ca7a032d26b64a7b5a79945f5e222b1
stevensaw99 <stevensaw1999@gmail.com>
2022-09-01 19:43:18 -0700

    activity1

--- src/main/java/com/unicorn/Calculator.java
+++ src/main/java/com/unicorn/Calculator.java
@@ -1,2 +1,38 @@
-package com.unicorn;public class Calculator {
+package com.unicorn;
+
+import java.util.Arrays;
+
+import static java.util.Arrays.*;
+
+public class Calculator
+{
+
+
+    public int add(int a, int... b)
+    {
+        return a + Arrays.stream(b).sum();
+    }
+
+    public int add(int a, int b)
+    {
+
+        return 0;
+    }
+
+    public int subtract(int a, int b)
+    {
+        return a-b;
+    }
+
+    public int multiply(int a, int b)
+    {
+        return a * b;
+
+    }
+
+    public int divide(int a, int b)
+    {
+        return a / b;
+    }
+
 }
